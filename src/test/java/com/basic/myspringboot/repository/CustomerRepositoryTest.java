a95db1bea8de4406997ab704be7f958066762b52
WorkSpaceForHanmg <dlzkfmtm11@naver.com>
2025-05-07 17:02:06 +0900

    test update

--- src/test/java/com/basic/myspringboot/repository/CustomerRepositoryTest.java
+++ src/test/java/com/basic/myspringboot/repository/CustomerRepositoryTest.java
@@ -1,23 +1,49 @@
 package com.basic.myspringboot.repository;
 
 import com.basic.myspringboot.entity.Customer;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
+//@DataJpaTest
 class CustomerRepositoryTest {
     @Autowired
     CustomerRepository customerRepository;
 
     @Test
+    void testFindBy() {
+        Optional<Customer> optionalCustomer = customerRepository.findById(1L);
+//        assertThat(optionalCustomer).isNotEmpty();
+        if (optionalCustomer.isPresent()) {
+            Customer existCustomer = optionalCustomer.get();
+            assertThat(existCustomer.getId()).isEqualTo(1L);
+        }
+        //Optional 의 T orElseGet(Supplier<? extneds T> supplier)
+        //Supplier 의 추상메서드 T get()
+        //고객 번호가 존재하는 경우
+        Optional<Customer> optionalCustomer2 = customerRepository.findByCustomerId("A001");
+        Customer a001customer = optionalCustomer.orElseGet(() -> new Customer());
+        assertThat(a001customer.getCustomerName()).isEqualTo("스프링");
+
+        //고객 번호가 존재하지 않는 경우
+        Customer notFoundCustomer = customerRepository.findByCustomerId("A004")
+                .orElseGet(() -> new Customer());
+        assertThat(notFoundCustomer.getCustomerName()).isNull();
+    }
+    @Test
     @Rollback(value = false) //Rollback 처리하지 마라
+    @Disabled
     void testCreateCustomer() {
         //Given (준비 단계)
         Customer customer = new Customer();
